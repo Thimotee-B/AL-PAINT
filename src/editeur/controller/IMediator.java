cc593e0da368de439a760dff4af1c5d4e744581b
Yukii <alexisperignon@live.FR>
2020-04-07 09:57:18 +0200

    Interface minimale mediator

---
+++ src/editeur/controller/IMediator.java
@@ -0,0 +1,14 @@
+package editeur.controller;
+
+public interface IMediator {
+	
+	void group();
+	
+	void unGroup();
+	
+	void reColor();
+	
+	void ReScale();
+	
+	void rotate();
+}
---
+++ src/editeur/controller/MediatorJavaFx.java
@@ -0,0 +1,72 @@
+package editeur.controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MediatorJavaFx implements MouseListener, IMediator {
+	//On peut en faire un singleton si on veut
+	
+	@Override
+	public void group() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unGroup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reColor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ReScale() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotate() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	//Pas obligé mais ça peut être une solution pour la souris
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
