736529949f97172fba9db08fb96ffbee56d735a8
Yukii <alexisperignon@live.FR>
2020-05-03 21:28:08 +0200

    Update IMediator.java

--- src/editeur/controller/IMediator.java
+++ src/editeur/controller/IMediator.java
@@ -35,13 +35,6 @@ public interface IMediator extends SubjectObserve {
 	void MouseDraggedEvent(boolean fromToolbar ,int clickSide,Point old, Point to);
 	void MouseClickEventAddTool(boolean fromToolbar ,int clickSide,Point old, Point to);
 
-	@Override
-	void Attach(GraphicalObjectObserver observer);
 
-	@Override
-	void Detach(GraphicalObjectObserver observer);
-
-	@Override
-	void Notify();
 
 }
